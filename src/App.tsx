8158eb7b1e13c95b9f0ad8da4a1cda0b8ea6e3ef
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-07-23 18:24:30 +0100

    Added timestamp for each image, which is displayed when it's replayed

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,17 @@ import {
 	onMount,
 	onCleanup,
 	createSignal,
+	on,
 } from "solid-js";
 // The time (in ms) between each frame
 const SAMPLEINTERVAL = 1000;
 const PLAYBACKINTERVAL = 100;
+const formatTime = (time: number) => {
+	const date = new Date(time);
+	const timeStr = date.toTimeString().slice(0, 8);
+	const dateStr = date.toDateString();
+	return `${dateStr} ${timeStr}`;
+};
 const App: Component = () => {
 	// Canvas used for rendering content for the user
 	let displayCanvas: HTMLCanvasElement | undefined;
@@ -18,7 +25,8 @@ const App: Component = () => {
 
 	const videoElement: HTMLVideoElement = document.createElement("video");
 	// The current timelapse frames
-	const frames: ImageData[] = [];
+	const frames: { img: ImageData; timestamp: number }[] = [];
+	const [currentFrameIndex, setCurrentFrameIndex] = createSignal(0);
 	const [framesExist, setFramesExist] = createSignal(false);
 	// Set up video element
 	videoElement.autoplay = true;
@@ -41,6 +49,8 @@ const App: Component = () => {
 		videoElement.height = videoSettings.height!;
 		processingCanvas!.width = videoSettings.width!;
 		processingCanvas!.height = videoSettings.height!;
+		displayCanvas!.width = videoSettings.width!;
+		displayCanvas!.height = videoSettings.height!;
 		// Get context
 		const ctx = processingCanvas!.getContext("2d", {
 			willReadFrequently: true,
@@ -56,7 +66,7 @@ const App: Component = () => {
 				videoElement.height
 			);
 			if (!imgData) return;
-			frames.push(imgData);
+			frames.push({ img: imgData, timestamp: Date.now() });
 			setFramesExist(true);
 		}, SAMPLEINTERVAL);
 		onCleanup(() => clearInterval(interval));
@@ -64,25 +74,34 @@ const App: Component = () => {
 	// Playing back captured frames as a timelapse
 	createEffect(() => {
 		if (!framesExist()) return;
-		let currentFrameInd = 0;
-		displayCanvas!.width = frames[0].width;
-		displayCanvas!.height = frames[0].height;
 		const ctx = displayCanvas?.getContext("2d");
 		const interval = setInterval(() => {
 			if (!ctx || !frames.length) return;
 			// Loop index back to 0 when we reach the end of the frames array
-			if (currentFrameInd >= frames.length) currentFrameInd = 0;
-			const frame = frames[currentFrameInd];
-			ctx.beginPath();
-			ctx.clearRect(0, 0, frame.width, frame.height);
-			ctx.putImageData(frame, 0, 0);
-			currentFrameInd++;
+			let tmp = currentFrameIndex();
+			if (tmp + 1 >= frames.length) tmp = -1;
+			setCurrentFrameIndex(tmp + 1);
 		}, PLAYBACKINTERVAL);
 		onCleanup(() => clearInterval(interval));
 	});
+	createEffect(
+		on(currentFrameIndex, (i) => {
+			const frame = frames[i];
+			const ctx = displayCanvas?.getContext("2d");
+			if (!frame || !ctx) return;
+			ctx.beginPath();
+			ctx.clearRect(0, 0, frame.img.width, frame.img.height);
+			ctx.putImageData(frame.img, 0, 0);
+		})
+	);
 	return (
 		<Suspense>
-			<canvas ref={displayCanvas}></canvas>
+			<div class="absolute">
+				<div class="fixed top-8 right-8 text-xl">
+					Date: {formatTime(frames[currentFrameIndex()]?.timestamp)}
+				</div>
+				<canvas ref={displayCanvas}></canvas>
+			</div>
 		</Suspense>
 	);
 };
